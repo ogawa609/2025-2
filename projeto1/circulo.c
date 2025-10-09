49a336e564eb6c4410fc1138b36eead0755797a7
Eduardo Ogawa <eduardovitorogawa@gmail.com>
2025-10-09 09:50:53 -0300

    Add files via upload

---
+++ projeto1/circulo.c
@@ -0,0 +1,11 @@
+#include "circulo.h"
+
+float Area(float raio){
+
+    return 4*PI*(raio*raio);
+}
+
+float Volume (float raio){
+
+    return (4*PI*(raio*raio*raio))/3;
+}
---
+++ projeto1/circulo.h
@@ -0,0 +1,9 @@
+#ifndef _CIRCULO_H_
+#define _CIRCULO_H_
+
+#define PI 3.14159
+
+float Area (float R);
+float Volume (float R);
+
+#endif
